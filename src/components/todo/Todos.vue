ecd3e8f7da2ff4580ef2d41a200cb5ed7f38e529
Grégory Clain <g.clain69@gmail.com>
2022-09-30 13:08:27 +0200

    add todo component

--- src/components/todo/Todos.vue
+++ src/components/todo/Todos.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="mx-auto text-center text-3xl font-bold underline">
-    Hello
+  <div class="mx-auto">
+    <div class="text-center">
+      <Heading1>Just another todo app</Heading1>
+    </div>
+    <div class="shadow-lg mt-8 max-w-lg mx-auto">
+
+    </div>
   </div>
 </template>
 <script setup>
+import Heading1 from "../typography/Heading1.vue";
 
 </script>
---
+++ src/components/typography/Heading1.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-4xl font-extrabold"><slot></slot></h1>
+</template>
